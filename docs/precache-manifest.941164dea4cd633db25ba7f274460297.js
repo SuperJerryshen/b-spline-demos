26248c61114ef00349bb9c94965d608b53adc098
沈经纬 <327538014@qq.com>
2019-12-06 19:41:04 +0800

    chore: 更改打包时PUBLIC_URL配置

---
+++ docs/precache-manifest.941164dea4cd633db25ba7f274460297.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17237c846dc0f553ad48b9ec46aadc09",
+    "url": "/b-spline-demos/index.html"
+  },
+  {
+    "revision": "9f86ea7f05be5cdd0cb3",
+    "url": "/b-spline-demos/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "04e02535db8236700067",
+    "url": "/b-spline-demos/static/js/2.d78785e1.chunk.js"
+  },
+  {
+    "revision": "9f86ea7f05be5cdd0cb3",
+    "url": "/b-spline-demos/static/js/main.32b0dfc8.chunk.js"
+  },
+  {
+    "revision": "65aee192966cfad865a9",
+    "url": "/b-spline-demos/static/js/runtime-main.aae70b5b.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.56bbac775c44ba8ef2e095111f5acd7d.js"
+  "/b-spline-demos/precache-manifest.941164dea4cd633db25ba7f274460297.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/b-spline-demos/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
